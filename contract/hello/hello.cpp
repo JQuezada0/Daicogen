73385f53c5a92ef5695be944a1d57c811fa5fb42
JQuezada0 <johnil@blissfully.com>
2018-09-03 14:49:35 -0400

    Update cmake to use c++ standard 14

--- contract/hello/hello.cpp
+++ contract/hello/hello.cpp
@@ -9,6 +9,18 @@ class hello : public eosio::contract {
       void hi( account_name user ) {
          print( "Hello, ", name{user} );
       }
+
+    struct test_table {
+        uint64_t     id;
+        uint64_t     value = 0;
+        account_name owner;
+
+        auto primary_key()const { return id; }
+        uint64_t getValue()const { return value; }
+        account_name get_owner()const { return owner; }
+
+        EOSLIB_SERIALIZE( test_table, (id)(value)(owner) )
+    };
 };
 
 EOSIO_ABI( hello, (hi) )
