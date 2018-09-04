a84277dd7a248ae7fed085640cc7b27e36b703c7
JQuezada0 <johnil@blissfully.com>
2018-09-04 09:55:21 -0400

    WIP on sample db to write Frontend queries against

--- contract/hello/hello.cpp
+++ contract/hello/hello.cpp
@@ -1,13 +1,20 @@
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/multi_index.hpp>
 using namespace eosio;
 
 class hello : public eosio::contract {
   public:
       using contract::contract;
 
-      /// @abi action 
+      struct testValue {
+          uint64_t     id;
+          uint64_t     value = 0;
+          account_name owner;
+      };
+
+      /// @abi action
       void hi( account_name user ) {
-         print( "Hello, ", name{user} );
+        print("Hello, ", name{user});
       }
 
     struct test_table {
@@ -15,12 +22,25 @@ class hello : public eosio::contract {
         uint64_t     value = 0;
         account_name owner;
 
-        auto primary_key()const { return id; }
+        auto primaryKey()const { return id; }
         uint64_t getValue()const { return value; }
-        account_name get_owner()const { return owner; }
+        account_name getOwner()const { return owner; }
 
         EOSLIB_SERIALIZE( test_table, (id)(value)(owner) )
     };
+
+    eosio::multi_index<
+            N(test), // Name of the table
+            test_table, // Struct that represents the table
+            indexed_by<N(id), const_mem_fun<test_table, uint64_t, &test_table::primaryKey>>, // Query by id
+            indexed_by<N(value), const_mem_fun<test_table, uint64_t, &test_table::getValue>>, // Query by value
+            indexed_by<N(owner), const_mem_fun<test_table, uint64_t, &test_table::getOwner>> // Query by owner
+    > tests(N(h), N(t));
+
+    /// @abi action
+    void insert(testValue val) {
+      auto res = tests.emplace()
+    }
 };
 
 EOSIO_ABI( hello, (hi) )
